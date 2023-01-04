54c0adea2c5419c6f04a58ae01bc52f2fe14b709
Luke <86468152+LukeBlueLOx@users.noreply.github.com>
2023-01-04 09:12:46 +0100

    Update MRI.py

--- MRI.py
+++ MRI.py
@@ -102,7 +102,7 @@ fig.update_layout(
 
 fig.show()
 
-import plotly.graph_objects as go # or plotly.express as px
+"""import plotly.graph_objects as go # or plotly.express as px
 fig = go.Figure() # or any Plotly Express function e.g. px.bar(...)
 # fig.add_trace( ... )
 # fig.update_layout( ... )
@@ -116,4 +116,4 @@ app.layout = html.Div([
     dcc.Graph(figure=fig)
 ])
 
-app.run_server(debug=True, use_reloader=False)  # Turn off reloader if inside Jupyter
+app.run_server(debug=True, use_reloader=False)  # Turn off reloader if inside Jupyter"""
